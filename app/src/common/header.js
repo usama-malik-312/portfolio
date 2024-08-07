8f69b555fe7a207542a4c01ae846b03dcf563bf8
usama-bin-tahir <usamabintahir312@gmail.com>
2024-08-07 11:32:48 -0700

    appbar set

--- app/src/common/header.js
+++ app/src/common/header.js
@@ -36,104 +36,99 @@ const ButtonAppBar = () => {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, }}>
   <AppBar 
-    position="fixed"
+  position="fixed"
+  sx={{
+    backgroundColor:"#111827",
+    color:"#fff",
+    boxShadow:'none',
+    fontSize:"16px",
+  }}
+>
+  <Toolbar
     sx={{
-      backgroundColor: "#111827",
-      color: "#fff",
-      boxShadow: 'none',
-      fontSize: "16px",
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      height:"80px",
+      fontFamily:"Oswald, Arial"
     }}
   >
-    <Toolbar
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: "80px",
-        fontFamily: "Oswald, Arial"
-      }}
+    <IconButton
+      size="large"
+      edge="start"
+      color="inherit"
+      aria-label="menu"
+      sx={{ mr: 2, display: { xs: 'block', md: 'none' } }}
+      onClick={toggleDrawer(true)}
     >
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ 
-          display: { xs: 'block', md: 'none' }, 
-          mr: 2 
-        }}
-        onClick={() => toggleDrawer(true)}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Box 
-        sx={{ 
-          display: 'flex', 
-          alignItems: 'center',
-          cursor: 'pointer',
-          fontWeight: 700,
-        }}
-        onClick={handleScrollToTop}
-      >
-        <img src="/logo.svg" alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
-        <Typography variant="h6" component="div" sx={{ fontFamily: "Poppins", fontWeight: 700 }}>
-          Usama Bin Tahir
-        </Typography>
-      </Box>
-      <Box sx={{ 
-        display: { xs: 'none', md: 'flex' },
+      <MenuIcon />
+    </IconButton>
+    <Box 
+      sx={{ 
+        display: 'flex', 
         alignItems: 'center',
+        cursor: 'pointer',
         fontWeight: 700,
-      }}>
-        {menuItems.map((item) => (
-          <Button color="inherit" key={item.text}>
-            <ScrollLink to={item.to} smooth={true} duration={500} offset={-70}>
-              <Typography sx={{ fontFamily: "Poppins", fontWeight: 600 }}>
-                {item.text}
-              </Typography>
-            </ScrollLink>
-          </Button>
-        ))}
-      </Box>
-    </Toolbar>
-  </AppBar>
-
-  <Drawer anchor="left" open={drawerOpen} onClose={() => toggleDrawer(false)}>
-    <Box
-      role="presentation"
-      onClick={() => toggleDrawer(false)}
-      onKeyDown={() => toggleDrawer(false)}
-      sx={{
-        backgroundColor: "#111827",
-        color: "#fff",
-        boxShadow: 'none',
-        fontSize: "16px",
-        width: 250,
-        fontFamily: "Poppins", 
-        fontWeight: 700
       }}
+      onClick={handleScrollToTop}
+      
     >
-      <List>
-        {menuItems.map((item) => (
-          <ListItem button key={item.text}>
-            <ScrollLink
-              to={item.to}
-              smooth={true}
-              duration={500}
-              offset={-70}
-              onClick={() => toggleDrawer(false)}
-            >
-              <ListItemText primary={item.text} />
-            </ScrollLink>
-          </ListItem>
-        ))}
-      </List>
+      <img src="/logo.svg" alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
+      <Typography variant="h6" component="div" style={{fontFamily:"Poppins", fontWeight: 700}}>
+        Usama Bin Tahir
+      </Typography>
     </Box>
-  </Drawer>
-</Box>
+    <Box sx={{ 
+      display: { xs: 'none', md: 'flex' },
+      alignItems: 'center',
+      fontWeight: 700,
+    }}>
+      {menuItems.map((item) => (
+        <Button color="inherit" key={item.text}>
+          <ScrollLink to={item.to} smooth={true} duration={500} offset={-70}>
+            <p  style={{fontFamily:"Poppins", fontWeight: 600}}> {item.text}</p>
+          </ScrollLink>
+        </Button>
+      ))}
+    </Box>
+  </Toolbar>
+</AppBar>
 
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <Box
+          // sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+          sx={{
+            backgroundColor:"#111827",
+            color:"#fff",
+            boxShadow:'none',
+            fontSize:"16px",
+            width: 250,
+            fontFamily:"Poppins", fontWeight: 700
+          }}
+        >
+          <List>
+            {menuItems.map((item) => (
+              <ListItem button key={item.text}>
+                <ScrollLink
+                  to={item.to}
+                  smooth={true}
+                  duration={500}
+                  offset={-70}
+                  onClick={toggleDrawer(false)}
+                >
+                  <ListItemText primary={item.text} />
+                </ScrollLink>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
   );
 };
 
