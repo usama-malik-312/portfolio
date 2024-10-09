b1e515e73269128801596e762f84564f81a61fd9
usama-bin-tahir <usamabintahir312@gmail.com>
2024-10-09 10:40:42 -0700

    contact enables ui set for hero and about

--- app/src/components/About/about.js
+++ app/src/components/About/about.js
@@ -27,11 +27,6 @@ const experience = [
 
 const education = [
   { 
-    degree: 'MSAI', 
-    date: '2024 - Present', 
-    university: 'CUST Islamabad' 
-  },
-  { 
     degree: 'BSCS',
     date: '2017 - 2021', 
     university: 'UET Taxila' 
@@ -41,63 +36,61 @@ const education = [
 export default function About() {
   return (
     <div className={styles.aboutMain}>
-    <Container maxWidth="lg" >
+      <Container maxWidth="lg">
+        <Heading text={"About Me"} color={"#fff"} />
+        <div className={styles.about}>
+          <div className={styles.imageContainer}>
+            <div className={styles.imagediv}>
+              <Image
+                src="/usamaAboutNew.JPG" // Update with the actual path to your image
+                // src="/usamacar.jpeg" // Update with the actual path to your image
+                alt="About Image"
+                layout="responsive"
+                width={550}
+                height={330}
+                className={styles.aboutImg}
+              />
+            </div>
+          </div>
+          <div className={styles.description}>
+            <p>
+              Hi, I'm Usama Bin Tahir, a passionate software developer focused on delivering seamless and efficient solutions for the digital world. With experience in front-end and full-stack development, I specialize in building high-quality, user-friendly web applications. My journey in the tech industry includes working with reputable companies in Islamabad and Rawalpindi. I am always keen to learn new technologies and take on exciting challenges. Outside of work, I enjoy playing cricket and have a keen interest in cars.
+            </p>
 
-      <Heading text={"About Me"} color={"#fff"}></Heading>
-      <div className={styles.about}>
-        <div className={styles.imageContainer}>
-          <div className={styles.imagediv}>
-            <Image
-              src="/usamaAboutNew.JPG" // Update with the actual path to your image
-              alt="About Image"
-              layout="responsive"
-              width={550}
-              height={330}
-              className={styles.aboutImg}
-            />
+            <div className={styles.section}>
+              <h3>Experience</h3>
+              <ul>
+                {experience.map((item, index) => (
+                  <li key={index} className={styles.expSection}>
+                    <div className={styles.expDiv}>
+                      <div>{item.name}</div>  
+                      <div>{item.date}</div>
+                    </div>
+                    <div className={styles.expDiv}>
+                      <div>{item.role}</div>
+                      <div>{item.city}</div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className={styles.section}>
+              <h3>Education</h3>
+              <ul>
+                {education.map((item, index) => (
+                  <li key={index} className={styles.expSection}>
+                    <div className={styles.expDiv}>
+                      <div><strong>{item.degree}</strong> - {item.university}</div>  
+                      <div>{item.date}</div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-        <div className={styles.description}>
-          <p>
-            Hello, I'm Usama Bin Tahir. I'm a software developer with a passion for creating innovative solutions.
-            With expertise in various programming languages and frameworks, I strive to deliver high-quality software products.
-          </p>
-          <p>
-          Beyond the world of code, I enjoy playing cricket and have a keen interest in cars.</p>
-          <div>
-            <h3>Experience</h3>
-            <ul>
-              {experience.map((item, index) => (
-                <li key={index} className={styles.expSection}>
-                  <div className={styles.expDiv}>
-                    <div>{item.name}</div>  
-                    <div>{item.date}</div>
-                  </div>
-                  <div className={styles.expDiv}>
-                    <div>{item.role}</div>
-                    <div>{item.city}</div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h3>Education</h3>
-            <ul>
-              {education.map((item, index) => (
-                <li key={index} className={styles.expSection}>
-                  <div className={styles.expDiv}>
-                    <div><strong>{item.degree}</strong> - {item.university} </div>  
-                    <div>{item.date}</div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </Container>
-
+      </Container>
     </div>
   );
 }
--- app/src/components/Contact/contact.js
+++ app/src/components/Contact/contact.js
@@ -1,157 +1,173 @@
-import * as React from 'react';
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import styles from './contact.module.css'; // Import the CSS module for styling
+import * as React from "react";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import styles from "./contact.module.css"; // Import the CSS module for styling
 // import Heading from '../../common/Heading/heading';
-import ContactCard from '../../common/ContactCard/contactCard';
-import Heading from '../../common/Heading/heading';
-import { Container } from '@mui/material';
+import ContactCard from "../../common/ContactCard/contactCard";
+import Heading from "../../common/Heading/heading";
+import { Container } from "@mui/material";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    message: ''
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
-    console.log('Form data submitted:', formData);
+    console.log("Form data submitted:", formData);
+  };
+
+  const handleGmailClick = () => {
+    window.location.href = "mailto:usamabintahir312@gmail.com";
+  };
+
+  const handleLinkedInClick = () => {
+    window.open("https://www.linkedin.com/in/usama-bin-tahir-615aa4202", "_blank");
+  };
+
+  const handleGitHubClick = () => {
+    window.open("https://github.com/usama-bin-tahir", "_blank");
+  };
+
+  const handleWhatsAppClick = () => {
+    window.open("https://wa.me/923405254664", "_blank");
   };
 
   return (
-
     <div className={styles.skills}>
-     <Container maxWidth="lg" >
+      <Container maxWidth="lg">
+        <Heading text={"Contact Me"} color={"#000"} />
 
-    <Heading text={"Contact Me"} color={"#000"} />
+        <div className={styles.cardContainer}>
+        <ContactCard
+          icon="/gmail.svg"
+          heading="Email"
+          content="Let's get in touch"
+          onClick={handleGmailClick}
+        />
+        <ContactCard
+          icon="/linkedin.svg"
+          heading="LinkedIn"
+          content="Let's connect"
+          onClick={handleLinkedInClick}
+        />
+        <ContactCard
+          icon="/github.svg"
+          heading="Github"
+          content="Check out my repos"
+          onClick={handleGitHubClick}
+        />
+        <ContactCard
+          icon="/whatsapp.svg"
+          heading="WhatsApp"
+          content="Direct Message me"
+          onClick={handleWhatsAppClick}
+        />
+      </div>
+      </Container>
 
-    <div className={styles.cardContainer}>
-      <ContactCard
-        icon="/gmail.svg"
-        heading="Email"
-        content="Let's get in touch"
-      />
-      <ContactCard
-        icon="/linkedin.svg"
-        heading="LinkedIn"
-        content="Let's connect"
-      />
-      <ContactCard
-        icon="/github.svg"
-        heading="Github"
-        content="Checkout my repos"
-      />
-      <ContactCard
-        icon="/whatsapp.svg"
-        heading="WhatsApp"
-        content="Direct Message me"
-      />
+      {/* Contact Form */}
+      <div className={styles.contactFormDiv}>
+        {/* <Container maxWidth="lg" > */}
+
+        <form className={styles.contactForm} onSubmit={handleSubmit}>
+          <Container maxWidth="lg">
+            <Heading text={"Send Me a Message"} color={"#fff"} />
+
+            <div className={styles.formRow}>
+              <div className={styles.formGroup}>
+                <label htmlFor="firstName" className={styles.inputLabel}>
+                  First Name <span className={styles.required}>*</span>
+                </label>
+                <TextField
+                  id="firstName"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required
+                  fullWidth
+                  margin="normal"
+                  className={styles.inputField}
+                  variant="outlined"
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="lastName" className={styles.inputLabel}>
+                  Last Name <span className={styles.required}>*</span>
+                </label>
+                <TextField
+                  id="lastName"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  required
+                  fullWidth
+                  margin="normal"
+                  className={styles.inputField}
+                  variant="outlined"
+                />
+              </div>
+            </div>
+            <div className={styles.fullWidthGroup}>
+              <label htmlFor="email" className={styles.inputLabel}>
+                Email <span className={styles.required}>*</span>
+              </label>
+              <TextField
+                id="email"
+                name="email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                fullWidth
+                margin="normal"
+                className={styles.inputField}
+                variant="outlined"
+              />
+            </div>
+            <div className={styles.fullWidthGroup}>
+              <label htmlFor="message" className={styles.inputLabel}>
+                Message <span className={styles.required}>*</span>
+              </label>
+              <TextField
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+                fullWidth
+                margin="normal"
+                multiline
+                rows={4}
+                className={styles.inputField}
+                variant="outlined"
+              />
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={styles.submitButton}
+            >
+              Submit
+            </Button>
+          </Container>
+        </form>
+        {/* </Container> */}
+      </div>
     </div>
-    </Container>
-
-    {/* Contact Form */}
-    <div className={styles.contactFormDiv}>
-     {/* <Container maxWidth="lg" > */}
-
-    <form className={styles.contactForm} onSubmit={handleSubmit}>
-    <Container maxWidth="lg" >
-      <Heading text={"Send Me a Message"} color={"#fff"} />
-
-      <div className={styles.formRow}>
-        <div className={styles.formGroup}>
-          <label htmlFor="firstName" className={styles.inputLabel}>
-            First Name <span className={styles.required}>*</span>
-          </label>
-          <TextField
-            id="firstName"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            required
-            fullWidth
-            margin="normal"
-            className={styles.inputField}
-            variant="outlined"
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="lastName" className={styles.inputLabel}>
-            Last Name <span className={styles.required}>*</span>
-          </label>
-          <TextField
-            id="lastName"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            required
-            fullWidth
-            margin="normal"
-            className={styles.inputField}
-            variant="outlined"
-          />
-        </div>
-      </div>
-      <div className={styles.fullWidthGroup}>
-        <label htmlFor="email" className={styles.inputLabel}>
-          Email <span className={styles.required}>*</span>
-        </label>
-        <TextField
-          id="email"
-          name="email"
-          type="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          fullWidth
-          margin="normal"
-          className={styles.inputField}
-          variant="outlined"
-        />
-      </div>
-      <div className={styles.fullWidthGroup}>
-        <label htmlFor="message" className={styles.inputLabel}>
-          Message <span className={styles.required}>*</span>
-        </label>
-        <TextField
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-          fullWidth
-          margin="normal"
-          multiline
-          rows={4}
-          className={styles.inputField}
-          variant="outlined"
-        />
-      </div>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        className={styles.submitButton}
-      >
-        Submit
-      </Button>
-    </Container>
-
-    </form>
-    {/* </Container> */}
-
-  </div>
-  </div>
   );
 }
